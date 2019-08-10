b088616f528e9281d8c83e697782ec2da149b84d
Boris Velikovich <kiwiboris@gmail.com>
2019-08-10 15:50:56 -0400

    Add more info to forecast.js.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (longitude, latitude, callback) => {
             callback("Unable to find location.", undefined)
          }
         else {
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + " chance of rain.")
+            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + " chance of rain. The high today is " + body.daily.data[0].temperatureHigh + "F. The low today is " + body.daily.data[0].temperatureLow + "F.")
         }
     })
 }
